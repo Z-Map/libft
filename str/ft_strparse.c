db26c2b94d6da1d86e41ed61939094a974c73a70
Map <blendfan@gmail.com>
2016-05-13 15:38:37 +0200

    add strparser

--- str/ft_strparse.c
+++ str/ft_strparse.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 23:47:10 by qloubier          #+#    #+#             */
-/*   Updated: 2016/05/10 23:54:46 by qloubier         ###   ########.fr       */
+/*   Updated: 2016/05/13 15:36:42 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,13 @@
 char		*ft_strparse(char *str, char s, int (**parser)(char *, void *),
 				void *param)
 {
+	int		ret;
+
+	ret = 0;
 	while (*str && *str != s)
 		++str;
-	if (*str == s)
-		while (parser[*()](*str))
-
+	if (*(str++) == s)
+		while ((ret = parser[*(str)](str, param)) > 0)
+			++str;
+	return (ret);
 }
