ce78ca9e5217b68985eda2c6bfa0ec2892a6b195
_Map_ <blendfan@gmail.com>
2016-10-03 21:47:14 +0200

    add pf len func

--- format/ft_pflen_unbr.c
+++ format/ft_pflen_unbr.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pflen_unbr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qloubier <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/01 02:43:11 by qloubier          #+#    #+#             */
-/*   Updated: 2016/10/02 01:21:52 by qloubier         ###   ########.fr       */
+/*   Updated: 2016/10/02 16:53:17 by map              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_format.h"
 
-int		ft_pflen_nbr(t_pfc *arg)
+int		ft_pflen_unbr(t_pfc *arg)
 {
 	return (0);
 }
