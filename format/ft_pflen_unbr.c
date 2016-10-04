0b2d478bfe9d49d1d361eadd62e1b735af61c1bf
_Map_ <blendfan@gmail.com>
2016-10-04 15:42:23 +0200

    modif pause

--- format/ft_pflen_unbr.c
+++ format/ft_pflen_unbr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pflen_unbr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qloubier <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/01 02:43:11 by qloubier          #+#    #+#             */
-/*   Updated: 2016/10/04 13:04:22 by qloubier         ###   ########.fr       */
+/*   Updated: 2016/10/04 15:41:51 by map              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		ft_pflen_unbr(t_pfc *arg)
 	size_t	len;
 
 	len = ft_jdigitlen(arg->arg);
+	arg->b_len = (int)len;
 	if ((arg->flag & PREC_SET) && (len < arg->precision))
 		len = arg->precision;
 	if ((arg->flag & FORCE_SIGN) || (arg->arg & (1ul << 63)))
@@ -24,6 +25,5 @@ int		ft_pflen_unbr(t_pfc *arg)
 	if ((arg->flag & ZERO_FILL) && !(arg->flag & LEFT_ALIGN)
 		&& (len < arg->minwidth))
 		len = arg->minwidth;
-	arg->b_len = (int)len;
 	return (0);
 }
