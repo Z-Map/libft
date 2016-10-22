f80783fd68b6b103bd72113fb343b2739530e750
Quentin <blendfan@gmail.com>
2016-10-22 22:32:12 +0200

    add ft_dprintf

--- include/libft_printf.h
+++ include/libft_printf.h
@@ -6,7 +6,7 @@
 /*   By: map <map@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/10 05:51:14 by map               #+#    #+#             */
-/*   Updated: 2016/10/21 20:45:59 by qloubier         ###   ########.fr       */
+/*   Updated: 2016/10/22 22:30:34 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,7 +166,7 @@ int						ft_printf_bwritec(t_pfb *b, char c, size_t len);
 int						ft_printf_bflush(t_pfb *b);
 
 int						ft_printf(const char *fstr, ...);
-int						ft_printf_fd(int fd, const char *fstr, ...);
+int						ft_dprintf(int fd, const char *fstr, ...);
 int						ft_print_buff(const char *fstr, int len, t_list **args);
 
 const static char		*g_pf_nullstr = "(null)";
