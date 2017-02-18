0752fa805abb4f529a3659079da9e33199a96c15
Map <blendfan@gmail.com>
2017-02-18 13:38:55 +0100

    add ft_memswp

--- include/libft_memory.h
+++ include/libft_memory.h
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/19 10:24:59 by qloubier          #+#    #+#             */
-/*   Updated: 2016/11/22 18:00:07 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/02/18 13:34:50 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 # include <string.h>
 # include "ft.h"
 
-void			ft_bzero (void *s, size_t n);
-void			*ft_memset (void *b, int c, size_t len);
-void			*ft_memcpy (void *dst, const void *src, size_t n);
-void			*ft_memccpy (void *dst, const void *src, int c, size_t n);
-void			*ft_memmove (void *dst, const void *src, size_t len);
-void			*ft_memchr (const void *s, int c, size_t n);
-int				ft_memcmp (const void *s1, const void *s2, size_t n);
-void			*ft_memdup (const void *s1, size_t n);
-void			*ft_memalloc (size_t size);
-void			ft_memdel (void **ap);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memset(void *b, int c, size_t len);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memswp(void *ping, void *pong, size_t len);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memdup(const void *s1, size_t n);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
 t_ui			ft_memalign(t_ui i);
 void			ft_ptabdel(void ***tptr);
 
