5c361913a2cb4131b778846f520bef97912c7a4c
_Map_ <blendfan@gmail.com>
2017-03-06 18:34:34 +0100

    fix name on sscanf

--- parse/ft_sfflag_opt.c
+++ parse/ft_sfflag_opt.c
@@ -6,13 +6,13 @@
 /*   By: map <map@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 15:25:44 by map               #+#    #+#             */
-/*   Updated: 2017/03/06 15:26:39 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/03/06 17:52:37 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_parse.h"
 
-const char		*ft_sfflag_nc(const char *c, t_sfb *b)
+const char		*ft_sfflag_opt(const char *c, t_sfb *b)
 {
 	if (!(b->arg.flag & SFF_IGNORE))
 		b->arg.flag |= SFF_OPTIONAL;
