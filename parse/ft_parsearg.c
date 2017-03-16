8167a00cc5097f1d881b74cb281f340381b4c782
Quentin <blendfan@gmail.com>
2017-03-16 19:08:34 +0100

    fix compile

--- parse/ft_parsearg.c
+++ parse/ft_parsearg.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/10 17:39:12 by qloubier          #+#    #+#             */
-/*   Updated: 2017/03/10 20:38:52 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/03/16 17:04:55 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ t_argret		*ft_parsearg(t_arg *arglst, int argc, char **argv, void *arg)
 	t_argret	*argp;
 
 	if (!(argp = malloc(sizeof(t_argret))) ||
-		!(argp->av = ft_stabndup(argv, argc)))
+		!(argp->av = ft_stabndup((const char **)(t_ul)argv, (size_t)argc)))
 		return (NULL);
 	argp->ac = argc;
+	(void)arglst;
+	(void)arg;
 	return (argp);
 }
