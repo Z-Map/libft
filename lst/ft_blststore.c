861b4a1ee149548f4cd28f180ec91d762522dd2b
Map <blendfan@gmail.com>
2017-06-08 02:34:40 +0200

    change blststore, it can take null as data

--- lst/ft_blststore.c
+++ lst/ft_blststore.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 18:39:30 by qloubier          #+#    #+#             */
-/*   Updated: 2017/04/08 02:15:04 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/06/08 02:32:25 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void		*ft_blststore(t_blst *blst, void *data)
 {
 	void	*ret;
 
-	if (!blst || !data)
+	if (!blst)
 		return (NULL);
 	while (blst->next && (blst->len == blst->bsize))
 		blst = blst->next;
@@ -28,7 +28,8 @@ void		*ft_blststore(t_blst *blst, void *data)
 	if (!blst)
 		return (NULL);
 	ret = (void *)((unsigned long)blst->data + (blst->len * blst->csize));
-	FT_MCPY(ret, data, blst->csize);
+	if (data)
+		FT_MCPY(ret, data, blst->csize);
 	blst->len += 1;
 	return (ret);
 }
