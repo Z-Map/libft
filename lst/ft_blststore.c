bdc075a0c8cf8bc2b29c8b34697462c9299ca500
Map <blendfan@gmail.com>
2017-06-08 02:36:12 +0200

    blststore bzero if data is NULL

--- lst/ft_blststore.c
+++ lst/ft_blststore.c
@@ -6,7 +6,7 @@
 /*   By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 18:39:30 by qloubier          #+#    #+#             */
-/*   Updated: 2017/06/08 02:32:25 by qloubier         ###   ########.fr       */
+/*   Updated: 2017/06/08 02:35:24 by qloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ void		*ft_blststore(t_blst *blst, void *data)
 	ret = (void *)((unsigned long)blst->data + (blst->len * blst->csize));
 	if (data)
 		FT_MCPY(ret, data, blst->csize);
+	else
+		FT_BZERO(ret, blst->csize);
 	blst->len += 1;
 	return (ret);
 }
