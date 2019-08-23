7ca7f838e40c8cf5256db2e85b3d3fba14c4da13
Quentin Loubière <blendfan@gmail.com>
2019-08-23 14:07:15 +0200

    Update neutronstar

--- neutronstar.vars.mk
+++ neutronstar.vars.mk
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    neutronstar.vars.mk                                :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: qloubier <qloubier@student.42.fr>          +#+  +:+       +#+         #
+#    By: map <map@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/25 19:00:26 by qloubier          #+#    #+#              #
-#    Updated: 2019/07/25 19:25:54 by qloubier         ###   ########.fr        #
+#    Updated: 2019/08/10 23:23:00 by map              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,7 @@
 
 # Public vraiable
 
-DEFAULT_PUBLIC_VARS = 
+DEFAULT_PUBLIC_VARS		= 
 
 ifndef INCDIRS
   INCDIRS				= include
@@ -41,12 +41,18 @@ ifndef OUT_TYPE
   DEFAULT_PUBLIC_VARS	+= OUT_TYPE
 endif
 
-# Optional public vraiable
+# Required public variable
 # PROJECT_NAME	=
+
+ifndef PROJECT_NAME
+  $(error Missing PROJECT_NAME variable)
+endif
+
+# Optional public vraiable
 # INCS			= 
 # SRCS			= 
-# LIBS			= 
-# LIBDIRS		= 
+# LIBS			=
+# LIBDIRS		=
 # LIBSHEADERS	=
 # OUT_NAME		= 
 
@@ -57,6 +63,7 @@ endif
 
 ifndef AUTO_RECURSIVE
   AUTO_RECURSIVE		= on
+endif
 
 # Optional private variable
 # P_IINCDIRS	=
@@ -64,44 +71,24 @@ ifndef AUTO_RECURSIVE
 # SUBMAKES		= 
 # NAME			= 
 
-# Environnement var
-BUILDIR			= build
-TARGETDIR		= .
-RENDERDIR		= $(BUILDIR)/render
-PROJECTPATH		:= $(CURDIR)
-OPSYS			:= $(shell uname -s)
-SILENT			= @
+# Compile environnement var
+BUILDIR			?= build
+TARGETDIR		?= .
+RENDERDIR		?= $(BUILDIR)/render
+LOGNAME			?=$(USER)
 CHRONOS_NAZI_MODE = on
 
-ifeq ($(SILENT),@)
-  FANCY_OUT		= on
-else
-  FANCY_OUT		= off
-endif
-
-ifndef LOGNAME
-  LOGNAME		=$(USER)
-endif
 
 # Compilation var
-ifndef CFLAGS
-  CFLAGS		= -pthread 
-endif
+CFLAGS			?=
+CC				?= clang
+LC				?= ar
+LFLAGS			?= -rcs
 
-ifndef CC
-  CC			= clang
-endif
-
-ifndef LC
-  LC			= ar
-endif
-
-LINK_MODE		= static
+LINK_MODE		?= static
 
 ifeq ($(CHRONOS_NAZI_MODE)_$(CC),on_clang)
-  CFLAGS		+= -Wextra -Werror -Weverything
+  REQUIERED_CFLAGS		+= $(filter-out $(REQUIERED_CFLAGS), -Wextra -Werror -Weverything)
 else
-  CFLAGS		+= -Wextra -Werror -Wall -Wpadded
+  REQUIERED_CFLAGS		+= $(filter-out $(REQUIERED_CFLAGS), -Wextra -Werror -Wall -Wpadded)
 endif
-
-LFLAGS			= -rcs
